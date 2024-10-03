1fa81c9fd33fd3eca8792c111f6beb28000ebd51
imtiyazag <imtiyazagasimani@gmail.com>
2024-10-03 19:30:09 +0530

    initial commit

---
+++ javascript.js
@@ -0,0 +1,36 @@
+const Display = document.getElementById('display');
+
+document.addEventListener('submit', function(){
+   
+   const ProductName = document.querySelector('#productName').value;
+   const ProductImage = document.querySelector('#productImage').value;
+   const ProductDescription = document.querySelector('#productDescription').value;
+
+
+   saveProduct(ProductName,ProductImage,ProductDescription);
+
+   blogCard();
+
+   form.reset();    
+})
+
+function saveProduct(name,image,desc){
+
+   const product = {
+       name,
+       image,
+       desc
+   };
+   localStorage.setItem('product', JSON.stringify(product));
+}
+
+function blogCard(){
+   const item = JSON.parse( localStorage.getItem('product'));   
+       Display.innerHTML = `<img src = "${item.image}">
+       <h2> ${item.name} </h2>
+       <h3> ${item.desc}</h3>
+       `;     
+}
+
+document.onload = blogCard();
+
